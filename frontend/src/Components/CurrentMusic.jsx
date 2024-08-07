549e7b4c638272277ee401ef76bb164d2f47bc18
Ikhang94 <ctkhang94@gmail.com>
2024-08-07 20:38:45 +0200

    feat : adapt sentence in CurrentMusic Component if you select a music or not

--- frontend/src/Components/CurrentMusic.jsx
+++ frontend/src/Components/CurrentMusic.jsx
@@ -14,13 +14,31 @@ const background = {
   backgroundColor: "#de1523",
 };
 
+
+const currentMusicStyle = {
+  display: "flex", 
+  flexDirection: "column",
+  justifyContent: "center", 
+  alignItems: 'center'
+}
+
 const CurrentMusic = ({ name, author }) => {
   return (
     <Container sx={background}>
-      <Box>
-        <Typography variant="h4">Current Music</Typography>
-        <Typography variant="h5">{name}</Typography>
-        <Typography variant="h6">by {author}</Typography>
+      <Box sx={currentMusicStyle}>
+        {name && author && (
+          <>
+            <Typography variant="h4">Current Music</Typography>
+            <Typography variant="h5">{name}</Typography>
+            <Typography variant="h6">by {author}</Typography>
+          </>
+        )}
+        {!name && !author && (
+          <>
+            <Typography variant="h6">Select your music</Typography>
+          </>
+        )}
+
       </Box>
     </Container>
   );
